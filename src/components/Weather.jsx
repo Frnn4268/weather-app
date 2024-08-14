7a38e9ff742e63c71f78f368821c70ceba827907
Frnn4268 <appstutorialesy@gmail.com>
2024-08-14 10:37:47 -0600

    Adding changes in Weather.jsx and Weather.css

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import "../css/Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
 import searc_icon from "../assets/search.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
   return (
     <div className="weather">
       <div className="search-bar">
         <input type="text" placeholder="Search" />
-        <img src={searc_icon} alt="" />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">16°c</p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6 Km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
       </div>
     </div>
   );
