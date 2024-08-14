f4aaf33fb546d5dd25061f84ae690dc3abfba91f
Frnn4268 <appstutorialesy@gmail.com>
2024-08-14 16:59:34 -0600

    Adding new responsive styles in DetailedWeather

--- src/components/DetailedWeather.jsx
+++ src/components/DetailedWeather.jsx
@@ -9,39 +9,27 @@ const DetailedWeather = ({ weatherData }) => {
 
   return (
     <div className="detailed-weather">
-      <h2>
-        Weather details for {weatherData.location}, {weatherData.country}
-      </h2>
-      <p>
-        <strong>Description:</strong> {weatherData.description}
-      </p>
-      <p>
-        <strong>Temperature:</strong> {weatherData.temperature}°C
-      </p>
-      <p>
-        <strong>Mumidity:</strong> {weatherData.humidity}%
-      </p>
-      <p>
-        <strong>Wind Velocity:</strong> {weatherData.windSpeed} m/s
-      </p>
-      <p>
-        <strong>Pressure:</strong> {weatherData.pressure} hPa
-      </p>
-      <p>
-        <strong>Latitude:</strong> {weatherData.latitude}
-      </p>
-      <p>
-        <strong>Length:</strong> {weatherData.longitude}
-      </p>
-      <p>
-        <strong>Surise:</strong>{" "}
-        {new Date(weatherData.sunrise * 1000).toLocaleTimeString()}
-      </p>
-      <p>
-        <strong>Sunset:</strong>{" "}
-        {new Date(weatherData.sunset * 1000).toLocaleTimeString()}
-      </p>
+  <h2>
+    Weather details for {weatherData.location}, {weatherData.country}
+  </h2>
+  <div className="weather-detail">
+    <div className="weather-column">
+      <p><strong>Description:</strong> {weatherData.description}</p>
+      <p><strong>Temperature:</strong> {weatherData.temperature}°C</p>
+      <p><strong>Humidity:</strong> {weatherData.humidity}%</p>
     </div>
+    <div className="weather-column">
+      <p><strong>Wind Velocity:</strong> {weatherData.windSpeed} m/s</p>
+      <p><strong>Pressure:</strong> {weatherData.pressure} hPa</p>
+      <p><strong>Latitude:</strong> {weatherData.latitude}</p>
+    </div>
+    <div className="weather-column">
+      <p><strong>Longitude:</strong> {weatherData.longitude}</p>
+      <p><strong>Sunrise:</strong> {new Date(weatherData.sunrise * 1000).toLocaleTimeString()}</p>
+      <p><strong>Sunset:</strong> {new Date(weatherData.sunset * 1000).toLocaleTimeString()}</p>
+    </div>
+  </div>
+</div>
   );
 };
 
