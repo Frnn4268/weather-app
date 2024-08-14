5fcbc41008c00592977b13bc5b83f819244fa4c6
Frnn4268 <appstutorialesy@gmail.com>
2024-08-13 21:45:40 -0600

    Adding new component called Weather.jsx and it's css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Weather from "./components/Weather";
 
 const App = () => {
-  return <div className="app">Test</div>;
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Weather.css";
+
+const Weather = () => {
+  return (
+    <div>
+      <h1>Weather</h1>
+    </div>
+  );
+};
+
+export default Weather;
