1d97bbc4ddbab49180d6eedccb7dd38c529ce260
Frnn4268 <appstutorialesy@gmail.com>
2024-08-14 10:54:47 -0600

    Adding new search function in Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../css/Weather.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png";
@@ -11,6 +11,22 @@ import wind_icon from "../assets/wind.png";
 import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+        import.meta.env.VITE_APP_WEATHER_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    search("London");
+  }, [])
+
   return (
     <div className="weather">
       <div className="search-bar">
