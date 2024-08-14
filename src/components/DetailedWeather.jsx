b29e19d8e4cd11a983a30d5de41119b7170ca81b
Frnn4268 <appstutorialesy@gmail.com>
2024-08-14 16:50:46 -0600

    Adding new .css file for DetailedWeather.jsx

--- src/components/DetailedWeather.jsx
+++ src/components/DetailedWeather.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../css/DetailedWeather.css"
+
 const DetailedWeather = ({ weatherData }) => {
   if (!weatherData) {
     return <div>There is not data available to show.</div>;
